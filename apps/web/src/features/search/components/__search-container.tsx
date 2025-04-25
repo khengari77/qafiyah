2465285084bd73a95aea56e8145588194a7b60e2
alwalxed <alwalxed@proton.me>
2025-04-26 00:31:02 +0300

    feat: enhanced search container

--- apps/web/src/features/search/components/__search-container.tsx
+++ apps/web/src/features/search/components/__search-container.tsx
@@ -51,97 +51,99 @@ export function SearchContainer() {
   } = useSearch();
 
   return (
-    <div className="w-full max-w-2xl mx-auto flex flex-col my-32 gap-10 md:gap-16" dir="rtl">
-      <h1 className="font-bold text-center justify-center items-center text-2xl xxs:text-3xl xs:text-4xl md:text-5xl text-zinc-800 flex py-2">
-        {text.currentHeaderTitle}
-      </h1>
-      <Card className="border-0 shadow-none bg-transparent">
-        <div className="p-0 bg">
-          <div className="flex flex-col gap-4">
-            <SearchInput
-              effectText={text.currentInputPlaceholder}
-              handleTypingEffect={handleTypingEffect}
-              hasSubmitted={hasSubmitted}
-              isLoading={isLoading}
-              searchLabel={text.search}
-              inputValue={inputValue}
-              validationError={validationError}
-              handleCustomKeyDown={handleCustomKeyDown}
-              handleCustomSearch={handleCustomSearch}
-              handleCustomInputChange={handleCustomInputChange}
-              resetAllStates={resetAllStates}
-            />
-            <div className="flex items-center justify-between">
-              <FiltersButton toggleFilters={toggleFilters} filtersVisible={filtersVisible} />
-
-              <FilterBadges
-                selectedErasLength={selectedEras.length}
-                selectedMetersLength={selectedMeters.length}
-                selectedRhymesLength={selectedRhymes.length}
-                selectedThemesLength={selectedThemes.length}
-                badgeErasCountText={text.badgeErasCount}
-                badgeMetersCountText={text.badgeMetersCount}
-                badgeThemesCountText={text.badgeThemesCount}
-                badgeRhymesCountText={text.badgeRhymesCount}
+    <div className="w-full mx-auto max-w-2xl min-h-[85svh] py-24 flex justify-center items-center">
+      <div className="w-full flex flex-col gap-10 md:gap-16" dir="rtl">
+        <h1 className="font-bold text-center justify-center items-center text-2xl xxs:text-3xl xs:text-4xl md:text-5xl text-zinc-800 flex py-2">
+          {text.currentHeaderTitle}
+        </h1>
+        <Card className="border-0 shadow-none bg-transparent">
+          <div className="p-0 bg">
+            <div className="flex flex-col gap-4">
+              <SearchInput
+                effectText={text.currentInputPlaceholder}
+                handleTypingEffect={handleTypingEffect}
+                hasSubmitted={hasSubmitted}
+                isLoading={isLoading}
+                searchLabel={text.search}
+                inputValue={inputValue}
+                validationError={validationError}
+                handleCustomKeyDown={handleCustomKeyDown}
+                handleCustomSearch={handleCustomSearch}
+                handleCustomInputChange={handleCustomInputChange}
+                resetAllStates={resetAllStates}
               />
+              <div className="flex items-center justify-between">
+                <FiltersButton toggleFilters={toggleFilters} filtersVisible={filtersVisible} />
+
+                <FilterBadges
+                  selectedErasLength={selectedEras.length}
+                  selectedMetersLength={selectedMeters.length}
+                  selectedRhymesLength={selectedRhymes.length}
+                  selectedThemesLength={selectedThemes.length}
+                  badgeErasCountText={text.badgeErasCount}
+                  badgeMetersCountText={text.badgeMetersCount}
+                  badgeThemesCountText={text.badgeThemesCount}
+                  badgeRhymesCountText={text.badgeRhymesCount}
+                />
+              </div>
+
+              {filtersVisible && (
+                <Filters
+                  searchType={searchType}
+                  searchTypeLabelText={text.searchTypeLabel}
+                  searchTypeOptions={searchTypeOptions}
+                  searchParamsSearchType={searchParams.search_type}
+                  handleCustomSearchTypeChange={handleCustomSearchTypeChange}
+                  searchTypePlaceholderText={text.searchTypePlaceholder}
+                  matchTypeLabelText={text.matchTypeLabel}
+                  matchTypeOptions={matchTypeOptions}
+                  searchParamsMatchType={searchParams.match_type}
+                  handleMatchTypeChange={handleMatchTypeChange}
+                  erasLabelText={text.erasLabel}
+                  erasOptions={erasOptions}
+                  selectedEras={selectedEras}
+                  erasPlaceholderNounFormsText={text.erasPlaceholderNounForms}
+                  handleErasChange={handleErasChange}
+                  erasPlaceholderText={text.erasPlaceholder}
+                  metersLabelText={text.metersLabel}
+                  metersOptions={metersOptions}
+                  selectedMeters={selectedMeters}
+                  metersPlaceholderNounFormsText={text.metersPlaceholderNounForms}
+                  handleMetersChange={handleMetersChange}
+                  metersPlaceholderText={text.metersPlaceholder}
+                  themesLabelText={text.themesLabel}
+                  themesOptions={themesOptions}
+                  selectedThemes={selectedThemes}
+                  themesPlaceholderNounFormsText={text.themesPlaceholderNounForms}
+                  handleThemesChange={handleThemesChange}
+                  themesPlaceholderText={text.themesPlaceholder}
+                  rhymesLabelText={text.rhymesLabel}
+                  rhymesOptions={rhymesOptions}
+                  selectedRhymes={selectedRhymes}
+                  rhymesPlaceholderNounFormsText={text.rhymesPlaceholderNounForms}
+                  handleRhymesChange={handleRhymesChange}
+                  rhymesPlaceholderText={text.rhymesPlaceholder}
+                />
+              )}
             </div>
-
-            {filtersVisible && (
-              <Filters
-                searchType={searchType}
-                searchTypeLabelText={text.searchTypeLabel}
-                searchTypeOptions={searchTypeOptions}
-                searchParamsSearchType={searchParams.search_type}
-                handleCustomSearchTypeChange={handleCustomSearchTypeChange}
-                searchTypePlaceholderText={text.searchTypePlaceholder}
-                matchTypeLabelText={text.matchTypeLabel}
-                matchTypeOptions={matchTypeOptions}
-                searchParamsMatchType={searchParams.match_type}
-                handleMatchTypeChange={handleMatchTypeChange}
-                erasLabelText={text.erasLabel}
-                erasOptions={erasOptions}
-                selectedEras={selectedEras}
-                erasPlaceholderNounFormsText={text.erasPlaceholderNounForms}
-                handleErasChange={handleErasChange}
-                erasPlaceholderText={text.erasPlaceholder}
-                metersLabelText={text.metersLabel}
-                metersOptions={metersOptions}
-                selectedMeters={selectedMeters}
-                metersPlaceholderNounFormsText={text.metersPlaceholderNounForms}
-                handleMetersChange={handleMetersChange}
-                metersPlaceholderText={text.metersPlaceholder}
-                themesLabelText={text.themesLabel}
-                themesOptions={themesOptions}
-                selectedThemes={selectedThemes}
-                themesPlaceholderNounFormsText={text.themesPlaceholderNounForms}
-                handleThemesChange={handleThemesChange}
-                themesPlaceholderText={text.themesPlaceholder}
-                rhymesLabelText={text.rhymesLabel}
-                rhymesOptions={rhymesOptions}
-                selectedRhymes={selectedRhymes}
-                rhymesPlaceholderNounFormsText={text.rhymesPlaceholderNounForms}
-                handleRhymesChange={handleRhymesChange}
-                rhymesPlaceholderText={text.rhymesPlaceholder}
-              />
-            )}
           </div>
-        </div>
-      </Card>
+        </Card>
 
-      <ResultList
-        data={data}
-        loadMoreRef={loadMoreRef}
-        isError={isError}
-        isFetchingNextPage={isFetchingNextPage}
-        isLoading={isLoading}
-        isSuccess={isSuccess}
-        inputValue={inputValue}
-        searchType={searchType}
-        errorMessageText={text.errorMessage}
-        refreshThePageText={text.refreshThePage}
-        noResultsFoundText={text.noResultsFound}
-        resultTextText={text.resultText}
-      />
+        <ResultList
+          data={data}
+          loadMoreRef={loadMoreRef}
+          isError={isError}
+          isFetchingNextPage={isFetchingNextPage}
+          isLoading={isLoading}
+          isSuccess={isSuccess}
+          inputValue={inputValue}
+          searchType={searchType}
+          errorMessageText={text.errorMessage}
+          refreshThePageText={text.refreshThePage}
+          noResultsFoundText={text.noResultsFound}
+          resultTextText={text.resultText}
+        />
+      </div>
     </div>
   );
 }
