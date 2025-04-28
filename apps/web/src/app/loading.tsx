7cf815624e7780fcfe72a6e2c2041d23637d9aaf
alwalxed <alwalxed@proton.me>
2025-04-28 12:13:16 +0300

    feat: tweak loading screen's height

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 
 export default function Loading() {
   return (
-    <div className="w-full min-h-svh flex items-center justify-center">
+    <div className="w-full min-h-[85svh] flex items-center justify-center">
       <Loader2 className={`${responsiveIconSize} text-zinc-800 animate-spin`} />
     </div>
   );
